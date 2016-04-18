c7a320f18acce84eff9b8198306ba61e2e24dca8
lee2rm <lee2rm@dukes.jmu.edu>
2016-04-17 22:38:23 -0400

    Added files via upload

---
+++ App_Code/QueryHandler.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+/// <summary>
+/// Summary description for QueryHandler
+/// </summary>
+public class QueryHandler
+{
+    public static ArrayList GetStudents(String parentID)
+    {
+        ArrayList studs = new ArrayList();
+
+        try
+        {
+            SqlConnection sc = new SqlConnection();
+            SqlCommand query = new SqlCommand();
+
+            sc.ConnectionString = @"Server = DESKTOP-QEKTMG0\LOCALHOST; Database = WBLDB; Trusted_Connection = Yes;";
+            sc.Open();
+
+            query.Connection = sc;
+            query.CommandText = "Select FirstName, LastName, EmailAddress " + 
+                "FROM GeneralUser " +
+                "Where EmailAddress = (" + 
+                "Select StudentEmailAddress from ParentStudent Where ParentEmailAddress = @ParentEmailAddress)";
+
+            query.Parameters.AddWithValue("@ParentEmailAddress", parentID);
+            Debug.WriteLine(query.CommandText);
+            Debug.WriteLine("Where @EmailAddress = " + parentID);
+            SqlDataReader read = query.ExecuteReader();
+
+            while (read.Read())
+            {
+                studs.Add(read.GetString(2));
+                studs.Add(read.GetString(0));
+                studs.Add(read.GetString(1));
+            
+            }
+            sc.Close();
+        }
+        catch (SqlException SQLe)
+        {
+            System.Diagnostics.Debug.Write(SQLe.ToString());
+        }
+
+        return studs;
+    }
+    
+    
+    public static ArrayList GetProfessors(String studentID)
+    {
+        ArrayList studs = new ArrayList();
+
+        try
+        {
+            SqlConnection sc = new SqlConnection();
+            SqlCommand query = new SqlCommand();
+
+            sc.ConnectionString = @"Server = DESKTOP-QEKTMG0\LOCALHOST; Database = WBLDB; Trusted_Connection = Yes;";
+            sc.Open();
+
+            query.Connection = sc;
+            query.CommandText = "Select EmailAddress, FirstName, LastName From GeneralUser Where" +
+                " EmailAddress = (Select EmailAddress From CourseStaff Where CourseID = (Select CourseID " +
+                "From Course Where CourseID = (Select CourseID From Attendance Where " +
+                "EmailAddress = @EmailAddress )))";
+
+            query.Parameters.AddWithValue("@EmailAddress", studentID);
+            Debug.WriteLine(query.CommandText);
+            Debug.WriteLine("Where @EmailAddress = " + studentID);
+            SqlDataReader read = query.ExecuteReader();
+
+            while (read.Read())
+            {
+                studs.Add(read.GetString(2));
+                studs.Add(read.GetString(0));
+                studs.Add(read.GetString(1));
+
+            }
+            sc.Close();
+        }
+        catch (SqlException SQLe)
+        {
+            System.Diagnostics.Debug.Write(SQLe.ToString());
+        }
+
+        return studs;
+    }
+    public static DropDownList PopDropDown(ArrayList info)
+    {
+        DropDownList ddl = new DropDownList();
+        int count = 0;
+        int size = (info.Count + 1) / 3;
+        for (int i = 0; i < size; i += 3)
+        {
+            String name = (String)info[i + 1] + " " + (String)info[i + 2];
+            ddl.Items.Insert(count, new ListItem(name, (String)info[i]));
+            count++;
+        }
+        
+        return ddl;
+    }
+}
---
+++ App_Code/WebActivity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using Twilio;
+
+/// <summary>
+/// Summary description for Activity
+/// </summary>
+public class WebActivity
+{
+    public static void LogActivity(string activity, bool recordPageUrl)
+    {
+
+
+        //string currentUser = (string)System.Web.HttpContext.Current.Session["userID"];
+        //string userType = (string)System.Web.HttpContext.Current.Session["userType"];
+        string currentUser = HttpContext.Current.Session["UserID"].ToString();
+        //string userType = "Administrator";
+        // Get information about the currently logged on user
+
+        if (currentUser != null)
+        {
+
+            // Log the activity in the database
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "dbo.LogUserActivity";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = connection;
+                cmd.Parameters.AddWithValue("@UserId", currentUser);
+                cmd.Parameters.AddWithValue("@Activity", activity);
+                if (recordPageUrl)
+                {
+                    cmd.Parameters.AddWithValue("@PageUrl", System.Web.HttpContext.Current.Request.RawUrl);
+                }
+
+                else
+                {
+                    cmd.Parameters.AddWithValue("@PageUrl", DBNull.Value);
+                }
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+    }
+    //Method for sending text message to users
+    public static void sendText(string message)
+    {
+        string AccountSid = "AC9096edb734a78078ee28b5bd13273756";
+        string AuthToken = "6b1a0e00aa260ba6f3bd8b483aed30a9";
+        Twilio.TwilioRestClient client = new Twilio.TwilioRestClient(AccountSid, AuthToken);
+
+
+        
+        // ACCOUNT_SID and ACCOUNT_TOKEN are from your Twilio account
+        var result = client.SendMessage("+12403033815", "+17039739495", message);
+        if (result.RestException != null)
+        {
+            Debug.WriteLine(result.RestException.Message);
+        }
+    }
+}
+
